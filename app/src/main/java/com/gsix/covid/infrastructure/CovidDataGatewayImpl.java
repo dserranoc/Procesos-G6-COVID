72702296ea364e69bc0fa14f294db6538e083f50
Serghei Sergheev <47123121+sergheevdev@users.noreply.github.com>
2020-10-27 15:48:59 +0100

    Clean up unused CovidDataGatewayImpl imports

--- app/src/main/java/com/gsix/covid/infrastructure/CovidDataGatewayImpl.java
+++ app/src/main/java/com/gsix/covid/infrastructure/CovidDataGatewayImpl.java
@@ -2,22 +2,9 @@ package infrastructure;
 
 import domain.CovidData;
 import infrastructure.util.JsonUtil;
-import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.JSONTokener;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.charset.Charset;
 import java.util.Date;
-import java.util.List;
-import java.util.Scanner;
 
 public class CovidDataGatewayImpl implements CovidDataGateway {
 
