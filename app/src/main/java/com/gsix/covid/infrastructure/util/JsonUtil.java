f9d8e2cac598ef26af54eb6a4b3607b59a42ac2f
Serghei Sergheev <47123121+sergheevdev@users.noreply.github.com>
2020-10-30 17:24:27 +0100

    Refactor variables readability

--- app/src/main/java/com/gsix/covid/infrastructure/util/JsonUtil.java
+++ app/src/main/java/com/gsix/covid/infrastructure/util/JsonUtil.java
@@ -16,20 +16,20 @@ public class JsonUtil {
     private static final Charset CHARSET = Charset.forName("UTF-8");
 
     public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
-        try (InputStream is = new URL(url).openStream()) {
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is, CHARSET));
-            String jsonText = readAll(rd);
+        try (InputStream input = new URL(url).openStream()) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input, CHARSET));
+            String jsonText = readAll(reader);
             return new JSONObject(jsonText);
         }
     }
 
-    private static String readAll(Reader rd) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        int cp;
-        while ((cp = rd.read()) != -1) {
-            sb.append((char) cp);
+    private static String readAll(Reader reader) throws IOException {
+        StringBuilder builder = new StringBuilder();
+        int character;
+        while ((character = reader.read()) != -1) {
+            builder.append((char) character);
         }
-        return sb.toString();
+        return builder.toString();
     }
 
 }
