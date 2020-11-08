0a08bef9e648a8cc3bd40c37519abc91e40e2df7
Daniel Serrano Cobos <d.serranoc.2017@alumnos.urjc.es>
2020-11-08 15:51:23 +0100

    changed menu style, added some icons and moved strings to strings.xml

--- app/src/main/java/com/gsix/covid/ui/home/HomeFragment.java
+++ app/src/main/java/com/gsix/covid/ui/home/HomeFragment.java
@@ -22,7 +22,7 @@ public class HomeFragment extends Fragment {
 
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
-        textView.setText("Codigo de la home");
+        textView.setText("In development!");
         return root;
     }
 }
