58106da6fca41e1022a295c5dbfa3302e58d42a0
Mrcsrobles <mrcsrobles@gmail.com>
2020-12-09 17:36:43 +0100

    Se ha creado la vista de quiz

--- app/src/main/java/com/gsix/covid/ui/quiz/QuizFragment.java
+++ app/src/main/java/com/gsix/covid/ui/quiz/QuizFragment.java
@@ -8,7 +8,6 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.gsix.covid.R;
 
@@ -17,9 +16,9 @@ public class QuizFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_test, container, false);
-        final TextView textView = root.findViewById(R.id.text_test);
-        textView.setText("In development!");
+        View root = inflater.inflate(R.layout.fragment_quiz, container, false);
+
+
 
         return root;
     }
