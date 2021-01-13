7f99390e352d15b02a421bd98f7f27bd4e032afe
Daniel Serrano Cobos <theroxmine@hotmail.es>
2021-01-13 20:38:34 +0100

    Pequeños ajustes

--- app/src/main/java/com/gsix/covid/ui/scores/ScoresFragment.java
+++ app/src/main/java/com/gsix/covid/ui/scores/ScoresFragment.java
@@ -1,15 +1,12 @@
 package com.gsix.covid.ui.scores;
 
-import android.annotation.SuppressLint;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.Pair;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -47,30 +44,47 @@ public class ScoresFragment extends Fragment {
 
 
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        params.setMargins(50, 50, 50, 50);
+        params.setMargins(50, 50, 50, 24);
         LinearLayout.LayoutParams params2 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
-        params2.setMargins(50, 20, 20, 20);
+        params2.setMargins(70, 20, 20, 20);
+        LinearLayout.LayoutParams params3 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        params3.setMargins(470, 20, 0, 20);
+        LinearLayout.LayoutParams params4 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        params4.setMargins(50,50,50,60);
 
         for (Pair<String, String> pair : scores) {
             CardView card = new CardView(getContext());
 
-            card.setCardBackgroundColor(getResources().getColor(R.color.design_default_color_on_primary));
+//            card.setCardBackgroundColor(getResources().getColor(R.color.design_default_color_on_primary));
+
 //            card.setBackground(getResources().getDrawable(R.drawable.gradientcards));
             card.setElevation(10);
             card.setRadius(20);
-//            if (pair.equals(scores.get(0))){
-//                params.setMargins(50,0,50,20);
-//            }
-
-            card.setLayoutParams(params);
 
             TextView text = new TextView(getContext());
-            String icon = (Integer.parseInt(pair.second) >= 8 / 2) ? "✅" : "❌";
-            text.setText(pair.second + "/8      " + pair.first + icon);
+            TextView text2 = new TextView(getContext());
+            text.setText(pair.second + "/8");
+            text2.setText(pair.first);
+            if (Integer.parseInt(pair.second) >= 8 / 2) {
+                card.setBackground(getResources().getDrawable(R.drawable.cardpassed));
+            } else {
+                card.setBackground(getResources().getDrawable(R.drawable.cardfailed));
+            }
+
             text.setTextSize(16);
             text.setId(pair.hashCode());
+            text2.setTextSize(16);
+            text2.setId(pair.hashCode());
+            if (pair.equals(scores.get(scores.size()-1))){
+                card.setLayoutParams(params4);
+            } else {
+                card.setLayoutParams(params);
+            }
+
             text.setLayoutParams(params2);
+            text2.setLayoutParams(params3);
             card.addView(text);
+            card.addView(text2);
             lLayout.addView(card);
         }
     }
